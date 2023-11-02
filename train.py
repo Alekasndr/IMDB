769249d0adbf6275ea214decbf0a1602d20b9c92
Alexander <leonovaleksandrwork@gmail.com>
2023-11-02 10:44:27 +0300

    Added test learning loop

--- train.py
+++ train.py
@@ -1,11 +1,26 @@
-import torch
 from torch.utils.data import DataLoader
 from torch.utils.data import TensorDataset
+from matplotlib import pyplot as plt
 
 from data_loader import *
+from model import *
+from utils import *
 
-# define batch size
-batch_size = 128
+# training config
+batch_size = 256
+lr = 0.001
+epochs = 8
+grad_clip = 5
+epochs = 8
+print_every = 1
+history = {
+    'train_loss': [],
+    'train_acc': [],
+    'val_loss': [],
+    'val_acc': [],
+    'epochs': epochs
+}
+es_limit = 5
 
 # create tensor datasets
 trainset = TensorDataset(torch.from_numpy(train_x), torch.from_numpy(train_y))
@@ -17,13 +32,129 @@ trainloader = DataLoader(trainset, shuffle=True, batch_size=batch_size)
 valloader = DataLoader(validset, shuffle=True, batch_size=batch_size)
 testloader = DataLoader(testset, shuffle=True, batch_size=batch_size)
 
-
 # check our batches
 dataiter = iter(trainloader)
 x, y = next(dataiter)
 
-print('Sample batch size: ', x.size())   # batch_size, seq_length
-print('Sample batch input: \n', x)
-print()
-print('Sample label size: ', y.size())   # batch_size
-print('Sample label input: \n', y)
+vocab_size = len(word2int)
+
+model = SentimentModel(vocab_size).to(device)
+print(model)
+
+loss_fn = nn.BCELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+epochloop = tqdm(range(epochs), position=0, desc='Training', leave=True)
+
+# early stop trigger
+es_trigger = 0
+val_loss_min = torch.inf
+
+for e in epochloop:
+
+    #################
+    # training mode #
+    #################
+
+    model.train()
+
+    train_loss = 0
+    train_acc = 0
+
+    for id, (feature, target) in enumerate(trainloader):
+        # add epoch meta info
+        epochloop.set_postfix_str(f'Training batch {id}/{len(trainloader)}')
+
+        # move to device
+        feature, target = feature.to(device), target.to(device)
+
+        # reset optimizer
+        optimizer.zero_grad()
+
+        # forward pass
+        out = model(feature)
+
+        # loss
+        loss = loss_fn(out.squeeze(), target.float())
+        train_loss += loss.item()
+        loss.backward()
+
+        # clip grad
+        nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
+
+        # update optimizer
+        optimizer.step()
+
+        # free some memory
+        del feature, target
+
+    history['train_loss'].append(train_loss / len(trainloader))
+    history['train_acc'].append(train_acc / len(trainloader))
+
+    ####################
+    # validation model #
+    ####################
+
+    model.eval()
+
+    val_loss = 0
+    val_acc = 0
+
+    with torch.no_grad():
+        for id, (feature, target) in enumerate(valloader):
+            # add epoch meta info
+            epochloop.set_postfix_str(f'Validation batch {id}/{len(valloader)}')
+
+            # move to device
+            feature, target = feature.to(device), target.to(device)
+
+            # forward pass
+            out = model(feature)
+
+
+            # loss
+            loss = loss_fn(out.squeeze(), target.float())
+            val_loss += loss.item()
+
+            # free some memory
+            del feature, target
+
+        history['val_loss'].append(val_loss / len(valloader))
+        history['val_acc'].append(val_acc / len(valloader))
+
+    # reset model mode
+    model.train()
+
+    # add epoch meta info
+    epochloop.set_postfix_str(f'Val Loss: {val_loss / len(valloader):.3f} | Val Acc: {val_acc / len(valloader):.3f}')
+
+    # print epoch
+    if (e + 1) % print_every == 0:
+        epochloop.write(
+            f'Epoch {e + 1}/{epochs} | Train Loss: {train_loss / len(trainloader):.3f} Train Acc: {train_acc / len(trainloader):.3f} | Val Loss: {val_loss / len(valloader):.3f} Val Acc: {val_acc / len(valloader):.3f}')
+        epochloop.update()
+
+    # save model if validation loss decrease
+    if val_loss / len(valloader) <= val_loss_min:
+        torch.save(model.state_dict(), './sentiment_lstm.pt')
+        val_loss_min = val_loss / len(valloader)
+        es_trigger = 0
+    else:
+        epochloop.write(
+            f'[WARNING] Validation loss did not improved ({val_loss_min:.3f} --> {val_loss / len(valloader):.3f})')
+        es_trigger += 1
+
+    # force early stop
+    if es_trigger >= es_limit:
+        epochloop.write(f'Early stopped at Epoch-{e + 1}')
+        # update epochs history
+        history['epochs'] = e + 1
+        break
+
+
+# plot loss
+plt.figure(figsize=(6, 8))
+plt.plot(range(history['epochs']), history['train_loss'], label='Train Loss')
+plt.plot(range(history['epochs']), history['val_loss'], label='Val Loss')
+plt.legend()
+plt.show()
